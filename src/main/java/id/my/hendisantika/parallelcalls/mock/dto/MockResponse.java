9b3ada622fc4e03817479b6ba7bf4259bf916d5c
hendisantika <hendisantika@yahoo.co.id>
2024-10-09 17:24:35 +0700

    Add MockResponse🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/parallelcalls/mock/dto/MockResponse.java
@@ -0,0 +1,21 @@
+package id.my.hendisantika.parallelcalls.mock.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-rest-template-parallel-calls
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 09/10/24
+ * Time: 17.24
+ * To change this template use File | Settings | File Templates.
+ */
+@Getter
+@Setter
+public class MockResponse {
+    private String id;
+    private String message;
+}
