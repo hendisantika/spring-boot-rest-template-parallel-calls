3390b684f828896caff8f6139db1be0f0737db76
hendisantika <hendisantika@yahoo.co.id>
2024-10-09 17:28:11 +0700

    Add fetchAllDataWithTimeout in ParallelService🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/parallelcalls/service/ParallelService.java
+++ src/main/java/id/my/hendisantika/parallelcalls/service/ParallelService.java
@@ -2,14 +2,18 @@ package id.my.hendisantika.parallelcalls.service;
 
 import id.my.hendisantika.parallelcalls.mock.dto.MockResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import java.util.stream.Stream;
 
 /**
@@ -22,6 +26,7 @@ import java.util.stream.Stream;
  * Time: 17.26
  * To change this template use File | Settings | File Templates.
  */
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ParallelService {
@@ -46,5 +51,29 @@ public class ParallelService {
                         .map(CompletableFuture::join)
                         .filter(Objects::nonNull)
                         .toList());
+    }
+
+    // Fetches data from multiple URLs in parallel with a global timeout and aggregates the results
+    public CompletableFuture<List<MockResponse>> fetchAllDataWithTimeout(long globalTimeout, long individualTaskTimeout) {
+        CompletableFuture<MockResponse> call1 = fetchDataWithTimeout("http://localhost:8080/mock/data/1", individualTaskTimeout);
+        CompletableFuture<MockResponse> call2 = fetchDataWithTimeout("http://localhost:8080/mock/data/2", individualTaskTimeout);
+        CompletableFuture<MockResponse> call3 = fetchDataWithTimeout("http://localhost:8080/mock/data/3", individualTaskTimeout);
+
+        return CompletableFuture.allOf(call1, call2, call3)
+                .orTimeout(globalTimeout, TimeUnit.MILLISECONDS)
+                .handle((result, ex) -> {
+                    if (ex != null) {
+                        if (ex instanceof TimeoutException) {
+                            log.error("Global timeout exception encountered");
+                        } else {
+                            log.error("Some other exception encountered at global level");
+                        }
+                        return Collections.emptyList();
+                    }
+                    return Stream.of(call1, call2, call3)
+                            .map(CompletableFuture::join)
+                            .filter(Objects::nonNull)
+                            .toList();
+                });
     }
 }
