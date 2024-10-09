9f5415008c9d0bc2abc42183144b7c5a3f2be5e2
hendisantika <hendisantika@yahoo.co.id>
2024-10-09 17:25:42 +0700

    Add getMockData in MockController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/parallelcalls/mock/controller/MockController.java
+++ src/main/java/id/my/hendisantika/parallelcalls/mock/controller/MockController.java
@@ -1,7 +1,10 @@
 package id.my.hendisantika.parallelcalls.mock.controller;
 
+import id.my.hendisantika.parallelcalls.mock.dto.MockResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,4 +24,17 @@ public class MockController {
 
     private static final Logger logger = LoggerFactory.getLogger(MockController.class);
 
+    // Simulates mock response with delay
+    @GetMapping("/data/{id}")
+    public MockResponse getMockData(@PathVariable String id) throws InterruptedException {
+        logger.info("Received a request for mock data with id: {}", id);
+
+        Thread.sleep(3000); // Adding a delay to simulate real-world scenarios
+
+        MockResponse response = new MockResponse();
+        response.setId(id);
+        response.setMessage("Mock data for ID " + id);
+        logger.info("Response sent for mock data with id: {}", id);
+        return response;
+    }
 }
