f5e83c5ae01e0ef42a9613599df13130aed87a23
hendisantika <hendisantika@yahoo.co.id>
2024-10-09 17:25:20 +0700

    Add MockController🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/parallelcalls/mock/controller/MockController.java
@@ -0,0 +1,24 @@
+package id.my.hendisantika.parallelcalls.mock.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-rest-template-parallel-calls
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 09/10/24
+ * Time: 17.25
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+@RequestMapping("/mock")
+public class MockController {
+
+    private static final Logger logger = LoggerFactory.getLogger(MockController.class);
+
+}
